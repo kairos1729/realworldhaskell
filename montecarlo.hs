5db87475b2e760a176b1f8c7c167297443d2cdcd
kairos1729 <kairos1729@users.noreply.github.com>
2015-06-16 14:21:47 +1000

    Added my own monad with a simple random number generator

--- montecarlo.hs
+++ montecarlo.hs
@@ -1,4 +1,4 @@
-module MonteCarlo (monteCarlo, cesaroTest, estimatePi) where
+module MonteCarlo (monteCarlo, cesaroTest, estimatePi, estimatePiMyRand) where
 import System.Random
 
 -- this is not from real world haskell, but I wanted to try
@@ -6,7 +6,7 @@ import System.Random
 -- purpose of that exercise was to show the benefits of
 -- assignment for modularity.  Haskell doesn't have assignment,
 -- so does this mean you cannot have modular programs?  No, I
--- think that modads are Haskell's answer to this.
+-- think that monads are Haskell's answer to this.
 -- The modular bit in the monte-carlo exercise was providing
 -- an experiment which returned true or false when it was called,
 -- but it was called with no arguments, so in Haskell this is not
@@ -35,3 +35,29 @@ estimatePi :: Int -> IO Double
 estimatePi numberOfTrials = do
   mc <- monteCarlo numberOfTrials cesaroTest
   return $ sqrt (6.0 / mc)
+
+randUpdate :: Int -> Int
+randUpdate x = ((27 * x) + 26) `mod` 127
+
+newtype RandomValue a = RandomValue (Int -> (a, Int))
+
+instance Monad RandomValue where
+  return v = RandomValue (\s -> (v, s))
+  RandomValue(rv) >>= f = RandomValue(newrandom)
+    where newrandom s = r newseed
+            where (result, newseed) = rv s
+                  RandomValue(r) = f result
+
+myCesaroTestMonad :: RandomValue Bool
+myCesaroTestMonad = RandomValue(myCesaroTest)
+
+myCesaroTest :: Int -> (Bool, Int)
+myCesaroTest s = ((gcd r1 r2) == 1, r2)
+  where r1 = randUpdate s
+        r2 = randUpdate r1
+
+estimatePiMyRand :: Int -> Int -> Double
+estimatePiMyRand seed numberOfTrials = sqrt (6.0 / result)
+  where RandomValue(mc) = monteCarlo numberOfTrials myCesaroTestMonad
+        (result, _) = mc seed
+  
