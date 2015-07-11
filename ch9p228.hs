51e603ac7f24709ec4b6ccca0392ab957659aeb1
kairos1729 <kairos1729@users.noreply.github.com>
2015-07-11 17:39:02 +1000

    added predicates

--- ch9p228.hs
+++ ch9p228.hs
@@ -1,10 +1,12 @@
 import System.Directory
-import System.Time
-import System.FilePath ((</>))
+import System.FilePath
 import Control.Monad
 import Control.Exception 
 import System.IO
 import Data.Time.Clock
+import Control.Applicative
+import Data.List
+import GHC.Exts
 
 main :: IO Integer
 main = return 0
@@ -46,4 +48,50 @@ getInfo path = do
   modified <- maybeIO (getModificationTime path)
   return (Info path perms size modified)
   
+list order = do
+  infos <- traverse order "."
+  forM infos (\i -> putStrLn $ show (infoPath i) ++ " " ++ show (infoSize i))
+  return ()
+  -- where
+    --order = (filter (maybe False (> 4000) . infoSize))
+    --order = filter isDirectory
+    --order = sortBy (\a b ->  transform a `compare` transform b)
 
+type InfoP a = Info -> a
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 o p1 p2 = o <$> p1 <*> p2
+
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP o p x = liftP2 o p (constP x)  
+
+constP :: a -> InfoP a
+constP x  = \_ -> x
+
+andP = liftP2 (&&)
+orP = liftP2 (||)
+
+equalP :: Eq a => InfoP a -> a -> InfoP Bool
+equalP = liftP (==) 
+
+greaterP :: Ord a => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath p = p . infoPath
+
+(==?) :: Eq a => InfoP a -> a -> InfoP Bool
+(==?) = equalP
+
+(&&?) = andP
+
+(>?) :: Ord a => InfoP a -> a -> InfoP Bool
+(>?) = greaterP
+
+sizeP :: Info -> Integer
+sizeP = maybe 0 id . infoSize
+
+myTest = (liftPath takeExtension ==? ".hs") &&? (sizeP >? 400)
+    
+    
+    
